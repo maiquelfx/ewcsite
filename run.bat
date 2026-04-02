60e56e3fb97fe58f2ffed11c045a06d8f6ebf049
maiquelfx <maiquelfx@gmail.com>
2026-04-02 15:00:16 -0300

    troca de imagem fundo preto 2026-04-02_15-00-16

--- run.bat
+++ run.bat
@@ -1,6 +1,8 @@
 @echo off
+set msg=troca de imagem fundo preto
 for /f %%i in ('powershell -Command "Get-Date -Format yyyy-MM-dd_HH-mm-ss"') do set dt=%%i
 
-C:\msys64\usr\bin\bash.exe -lc "cd /home/Win/git/ewcsite/ewcsite && git add . && git commit -m 'update %dt%' && git push"
+C:\msys64\usr\bin\bash.exe -lc "cd /home/Win/git/ewcsite/ewcsite && git add . && git commit -m '%msg% %dt%' && git push"
 
 pause
+
