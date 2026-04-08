3c71c761b670b9e219a9b26b8e8674e2baa9b78a
maiquelfx <maiquelfx@gmail.com>
2026-04-08 13:26:02 -0300

    fundo com efeitos no final da pagina fisica de particulas 2026-04-08_13-26-02

--- run.bat
+++ run.bat
@@ -1,5 +1,5 @@
 @echo off
-set msg=depoimentos foto user trocar do plano links novos secao sobre link linkedin
+set msg=fundo com efeitos no final da pagina fisica de particulas
 for /f %%i in ('powershell -Command "Get-Date -Format yyyy-MM-dd_HH-mm-ss"') do set dt=%%i
 
 C:\msys64\usr\bin\bash.exe -lc "cd /home/Win/git/ewcsite/ewcsite && git add . && git commit -m '%msg% %dt%' && git push"
