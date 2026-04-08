5b2e0f158c3fca71de937b9fe01bb651386de51c
maiquelfx <maiquelfx@gmail.com>
2026-04-08 12:44:06 -0300

    depoimentos foto user trocar do plano links novos secao sobre link linkedin 2026-04-08_12-44-05

--- run.bat
+++ run.bat
@@ -1,5 +1,5 @@
 @echo off
-set msg=troca de imagem fundo preto
+set msg=depoimentos foto user trocar do plano links novos secao sobre link linkedin
 for /f %%i in ('powershell -Command "Get-Date -Format yyyy-MM-dd_HH-mm-ss"') do set dt=%%i
 
 C:\msys64\usr\bin\bash.exe -lc "cd /home/Win/git/ewcsite/ewcsite && git add . && git commit -m '%msg% %dt%' && git push"
