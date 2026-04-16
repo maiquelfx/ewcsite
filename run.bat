c93ffd1efaceebab8d687351c42fbf6fdcc15168
maiquelfx <maiquelfx@gmail.com>
2026-04-16 17:49:37 -0300

    alteracao de precos precos anuais e mudanca de cores 2026-04-16_17-49-36

--- run.bat
+++ run.bat
@@ -1,5 +1,5 @@
 @echo off
-set msg=insercao de icon tentativa 2
+set msg=alteracao de precos precos anuais e mudanca de cores
 for /f %%i in ('powershell -Command "Get-Date -Format yyyy-MM-dd_HH-mm-ss"') do set dt=%%i
 
 C:\msys64\usr\bin\bash.exe -lc "cd /home/Win/git/ewcsite/ewcsite && git add . && git commit -m '%msg% %dt%' && git push"
