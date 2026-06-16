a975edbffe86d11974445cbba4a1af9c35e1f869
maiquelfx <maiquelfx@gmail.com>
2026-06-16 02:47:33 -0300

    mudanca de numero 2026-06-16_02-47-33

--- run.bat
+++ run.bat
@@ -1,5 +1,5 @@
 @echo off
-set msg=bandeiras e alteração no texto v2
+set msg=mudanca de numero
 for /f %%i in ('powershell -Command "Get-Date -Format yyyy-MM-dd_HH-mm-ss"') do set dt=%%i
 
 C:\msys64\usr\bin\bash.exe -lc "cd /home/Win/git/ewcsite/ewcsite && git add . && git commit -m '%msg% %dt%' && git push"
